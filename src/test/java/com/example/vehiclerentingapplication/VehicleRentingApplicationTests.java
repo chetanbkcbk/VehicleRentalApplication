9b8c4ada21c36d3c5f5e3aedd66eb6c415c6aaf8
Chetan <chetanbkcbk@gmail.com>
2024-12-10 15:40:07 +0530

    project creation

---
+++ src/test/java/com/example/vehiclerentingapplication/VehicleRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehiclerentingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
